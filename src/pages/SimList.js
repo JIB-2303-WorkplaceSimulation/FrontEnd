b1ec4196fa2e8be6887a9015b0e2b115308e0347
Karthik Sundar <karthiksgood@gmail.com>
2023-02-21 14:21:14 -0500

    filter based on simID functionality added

--- src/pages/SimList.js
+++ src/pages/SimList.js
@@ -1,6 +1,6 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import {Link, useParams} from 'react-router-dom';
-import dummy_data from './dummy_data.json';
+// import dummy_data from './dummy_data.json';
 import axios from 'axios';
 
 
@@ -8,16 +8,21 @@ import axios from 'axios';
 function SimList(){
   //console.log("this is dummy data, name: " + dummy_data.name);
   let params = useParams();
-  var id = params.simID;
+  var id = parseInt(params.simID);
   const [input, setInput] = useState([]);
 
-  axios.get("https://jsv2r3kn.directus.app/items/Furniture").then
+
+  useEffect(() => {
+    //Runs on the first render
+    //And any time any dependency value changes
+    axios.get("https://jsv2r3kn.directus.app/items/Furniture").then
   ((result) => {
      setInput(result.data.data);
     })
     .catch((err) => {
         console.log(err);
     });
+  }, [], input);
 
 
   const checkID = (inp) => {
@@ -48,8 +53,9 @@ function SimList(){
     
   }
 
+  console.log(input);
 
-  const DisplayData = input.map(
+  const DisplayData = input.filter(obj => obj.sim_id === id).map(
     (info)=>{
         return(
             <tr key={info.id}>
