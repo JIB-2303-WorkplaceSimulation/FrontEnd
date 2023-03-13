c5ba8be72e7f95be53abdf3652af8f516f8f27d8
Pranav Avula <pavula3@gatech.edu>
2023-03-12 22:30:39 -0400

    misc cleanup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,7 +27,7 @@ function Home(){
       <div className="center">
         <div className="box">
           <h1 style={{ color: 'black', fontSize: '28px' }}>Enter your Simulation ID to proceed</h1>
-          <p><input onChange={handleChange} onKeyDown={handleKey} value={input} placeholder="Enter Simulation ID Here"/></p>
+          <p><input onChange={handleChange} onKeyDown={handleKey} value={input} placeholder="Enter Simulation ID Here:"/></p>
           <p><button onClick={handleClick}>Submit</button></p>
           <p>(Only simulation ID 10 works at this time - entering other IDs will take us to a page with empty tables)</p>
         </div>
